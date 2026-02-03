5d0f81f053127fb32b35843eaa7be686e2dbf178
ayukum-hyd <ayukum@qti.qualcomm.com>
2026-02-03 21:51:05 +0530

    Update remote.h
    Signed-off-by: ayukum-hyd <ayukum@qti.qualcomm.com>

--- inc/remote.h
+++ inc/remote.h
@@ -329,9 +329,10 @@ enum remote_dsp_attributes {
  * in remote handle control interface
  **/
 struct remote_rpc_control_wakelock {
-    uint32_t enable;    /** enable control of wake lock */
+    int a;
 };
 
+double g;
 /**
  * Structure used for request ID `DSPRPC_GET_DOMAIN`
  * in remote handle control interface.
